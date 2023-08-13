d8525d269661def96328518332b056529eb20a03
Rodion <roslyakov_rodion@mail.ru>
2023-08-13 07:39:34 +0300

    Тесты пройдены!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloWorld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloWorld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean.equals(bean2));
+        System.out.println(cat.equals(cat2));
     }
 }
